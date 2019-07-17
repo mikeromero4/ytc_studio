13c604f8d9a0c28ed0fdd35ca0493610beee53a9
mikeromero4 <mikeromero4@yahoo.com>
2019-07-16 18:55:51 -0700

    comma
    added comma

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./styles.css";
 let developers = [
   { name: "Michael", description: "qrqwerwer", icon: "" },
   { name: "Yosias", description: "Graduated from CSUSB with Bachelor of Science in Computer Engineering", icon: "https://media.worldnomads.com/Explore/south-east-asia/raja-ampat-istock-lead.jpg" },
-  { name: "Hector", description: "I'm a cyber security student at CSUSB, I'm a part time barber and I have horse whos name is Hercules and a cat whos name is Tamale.", icon: "https://i.pinimg.com/originals/ac/fb/df/acfbdfa558edf7a0e5e57c1a93061e64.jpg" } 
+  { name: "Hector", description: "I'm a cyber security student at CSUSB, I'm a part time barber and I have horse whos name is Hercules and a cat whos name is Tamale.", icon: "https://i.pinimg.com/originals/ac/fb/df/acfbdfa558edf7a0e5e57c1a93061e64.jpg" }, 
   { name: "Ekay", description: "I'm a vibe Curator ", icon: "http://hubwav.com/wp-content/uploads/2017/10/Screen-Shot-2017-10-19-at-7.19.19-PM.png" },
 ];
 
