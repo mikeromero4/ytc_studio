2f81dc54920fea545256735d3f16ec4e3b129b69
yosiassapari <9287583+yosiassapari@users.noreply.github.com>
2019-07-16 18:23:04 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "./styles.css";
 
 let developers = [
   { name: "Michael", description: "qrqwerwer", icon: "" },
-  { name: "Yosias", description: "Graduated from CSUSB with Bachelor of Science in Computer Engineering", icon: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.worldnomads.com%2FExplore%2Fsouth-east-asia%2Fraja-ampat-istock-lead.jpg&imgrefurl=https%3A%2F%2Fwww.worldnomads.com%2Fexplore%2Fsoutheast-asia%2Findonesia%2Fexploring-west-papua&docid=AM3zL0yef77BwM&tbnid=o7cerQoh78BZ-M%3A&vet=10ahUKEwjcg47V4brjAhWV4J4KHQGqCcwQMwiOASgRMBE..i&w=750&h=490&bih=726&biw=1280&q=papua&ved=0ahUKEwjcg47V4brjAhWV4J4KHQGqCcwQMwiOASgRMBE&iact=mrc&uact=8" },
+  { name: "Yosias", description: "Graduated from CSUSB with Bachelor of Science in Computer Engineering", icon: "https://media.worldnomads.com/Explore/south-east-asia/raja-ampat-istock-lead.jpg" },
   { name: "Hector", description: "", icon: "" }
 ];
 
