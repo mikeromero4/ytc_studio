4cdd5bb98285e4459bc8723a65839c981f5186b5
mikeromero4 <mikeromero4@yahoo.com>
2019-07-16 18:02:11 -0700

    initial commit
    this is the first commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+let developers = [
+  { name: "Michael", description: "qrqwerwer", icon: "" },
+  { name: "Yosias", description: "", icon: "" },
+  { name: "Hector", description: "", icon: "" }
+];
+
+function UserList() {
+  let users = developers.map(e => {
+    return (
+      <div>
+        {e.name}
+        <br />
+        {e.description}
+        <img alt="userIcon" src={e.icon} />
+        <br /> <br />
+      </div>
+    );
+  });
+  return users;
+}
+
+function App() {
+  return (
+    <div>
+      welcome to ytc studios!! here's a list of our studio developers:
+      <br /> <br />
+      <UserList />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
