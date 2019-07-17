7387b96f21a48c792f8a20294064f77aa64b7969
EKAYYYY <52475847+EKAYYYY@users.noreply.github.com>
2019-07-16 18:39:56 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ let developers = [
   { name: "Michael", description: "qrqwerwer", icon: "" },
   { name: "Yosias", description: "Graduated from CSUSB with Bachelor of Science in Computer Engineering", icon: "https://media.worldnomads.com/Explore/south-east-asia/raja-ampat-istock-lead.jpg" },
   { name: "Hector", description: "I'm a cyber security student at CSUSB, I'm a part time barber and I have horse whos name is Hercules and a cat whos name is Tamale.", icon: "https://i.pinimg.com/originals/ac/fb/df/acfbdfa558edf7a0e5e57c1a93061e64.jpg" } 
-  { name: "Ekay", description: "I'm a  vibe Curator ", icon: "" },
+  { name: "Ekay", description: "I'm a vibe Curator ", icon: "" },
 ];
 
 function UserList() {
