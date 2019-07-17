cfe160311ab1763030d22a51c0921565efad08f0
EKAYYYY <52475847+EKAYYYY@users.noreply.github.com>
2019-07-16 18:38:25 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import "./styles.css";
 let developers = [
   { name: "Michael", description: "qrqwerwer", icon: "" },
   { name: "Yosias", description: "Graduated from CSUSB with Bachelor of Science in Computer Engineering", icon: "https://media.worldnomads.com/Explore/south-east-asia/raja-ampat-istock-lead.jpg" },
-  { name: "Hector", description: "I'm a cyber security student at CSUSB, I'm a part time barber and I have horse whos name is Hercules and a cat whos name is Tamale.", icon: "https://i.pinimg.com/originals/ac/fb/df/acfbdfa558edf7a0e5e57c1a93061e64.jpg" }
+  { name: "Hector", description: "I'm a cyber security student at CSUSB, I'm a part time barber and I have horse whos name is Hercules and a cat whos name is Tamale.", icon: "https://i.pinimg.com/originals/ac/fb/df/acfbdfa558edf7a0e5e57c1a93061e64.jpg" } 
+  { name: "Ekay", description: "I'm a  vibe Curator ", icon: "" },
 ];
 
 function UserList() {
