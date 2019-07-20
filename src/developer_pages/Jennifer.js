8cf76c1211eb929afb8cd1f13a29c841336db8b1
mikeromero4 <mikeromero4@yahoo.com>
2019-07-20 15:34:00 -0700

    added developers
    (Megan & Jennifer)

---
+++ src/developer_pages/Jennifer.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+function User() {
+  return (
+    <div>
+      welcome to my page!
+      <br />
+      My name is Jennifer
+    </div>
+  );
+}
+
+export default User;
